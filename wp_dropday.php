41400dab2157346cd6e6ec3acd5c83ece97c10b2
Matthijs <github@muynck.eu>
2025-12-05 16:19:35 +0100

    Add workflow to publish new releases on WordPress.org using SVN (#24)
    This workflow will submit a new tag to WordPress.org after a new release tag is published on GitHub.

--- wp_dropday.php
+++ wp_dropday.php
@@ -15,7 +15,7 @@
  * Developer URI: https://get.dropday.io/
  * Text Domain: dropday-for-woocommerce
  *
- * Copyright: © 2021 Dropday, Nl.
+ * Copyright: © 2024 Dropday, Nl.
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
